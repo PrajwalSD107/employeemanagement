26ed4a1978fda7800f8afe8ea3c568308ce13a88
PrajwalSD107 <prajwalkalaskar107@gmail.com>
2026-08-16 21:27:44 +0530

    chnage

---
+++ src/main/java/com/prajwal/employeemanagement/entity/prajwal.java
@@ -0,0 +1,20 @@
+package com.prajwal.employeemanagement.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class prajwal {
+
+
+     
+
+
+}
